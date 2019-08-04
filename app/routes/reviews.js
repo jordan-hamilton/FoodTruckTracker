8794418e49fd53badf4b001c2ff26ab620772d3f
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-08-04 12:14:43 -0600

    Add database queries to Handlebars context

--- app/routes/reviews.js
+++ app/routes/reviews.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request-promise-native');
 
 /* GET reviews page. */
 router.get('/', function(req, res, next) {
-    res.render('reviews', { title: 'FoodTruckTracker - Reviews' });
+    const host = req.protocol + '://' + req.get('host');
+    let context = {title: 'FoodTruckTracker - Reviews'};
+
+    request(`${host}/api/reviews`)
+        .then(function(body) {
+            context.review = JSON.parse(body);
+            return request(`${host}/api/food-trucks`);
+        })
+        .then(function(body) {
+            context.foodTruck = JSON.parse(body);
+            return request(`${host}/api/locations`);
+        })
+        .then(function(body) {
+           context.location = JSON.parse(body);
+           res.render('reviews', context);
+        })
+        .catch(function (err) {
+            console.error(err.stack);
+        });
 });
 
 module.exports = router;
