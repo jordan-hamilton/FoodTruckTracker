9bb449d178cafbeb4e1aa46fc5393aab1bfeb4ae
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-08-13 21:22:21 -0600

    Add express-session configuration, remove cookie-parser

--- app/app.js
+++ app/app.js
@@ -2,8 +2,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+
+var credentials = require('./credentials.js');
 
 // Import routes
 var indexRouter = require('./routes/index');
@@ -23,8 +25,16 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure sessions
+var expressSession = {
+  secret: credentials.secret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
+app.use(session(expressSession))
 
 // Configure routes
 app.use('/', indexRouter);
