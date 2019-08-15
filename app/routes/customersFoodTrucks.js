ccc2557bc1d68379331b48fb90a08805a5d6233a
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-08-15 15:44:31 -0600

    Add dropdown data to Handlebars context

--- app/routes/customersFoodTrucks.js
+++ app/routes/customersFoodTrucks.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request-promise-native');
 
 /* GET customers and food trucks page. */
 router.get('/', function(req, res, next) {
     const host = req.protocol + '://' + req.get('host');
     let context = {title: 'FoodTruckTracker - Reviews'};
 
-    res.render('customersFoodTrucks', context);
+    request(`${host}/api/reviews`)
+        .then(function(body) {
+            context.review = JSON.parse(body);
+            return request(`${host}/api/food-trucks`);
+        })
+        .then(function(body) {
+            context.foodTruck = JSON.parse(body);
+            return request(`${host}/api/customers`);
+        })
+        .then(function(body) {
+            context.customer = JSON.parse(body);
+            res.render('customersFoodTrucks', context);
+        })
+        .catch(function (err) {
+            console.error(err.stack);
+        });
 });
 
 module.exports = router;
